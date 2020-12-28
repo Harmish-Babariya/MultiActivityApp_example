df931ba6faf104a1fd5ea8f40750ad4db728852b
harmish babariya <harmishbabariya@gmail.com>
2020-12-28 17:34:56 +0530

    first commit

---
+++ app/src/main/java/com/example/multliactivity/OrderActivity.java
@@ -0,0 +1,21 @@
+package com.example.multliactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActivityChooserView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MSG);
+        TextView textView = findViewById(R.id.order);
+        textView.setText(message);
+    }
+}
